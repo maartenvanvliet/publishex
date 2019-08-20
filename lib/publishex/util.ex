8417de102cb197c1d0fc5756912946407ed43409
Maarten van Vliet <maartenvanvliet@gmail.com>
2019-08-20 20:36:03 +0200

    Document Util module

--- lib/publishex/util.ex
+++ lib/publishex/util.ex
@@ -1,10 +1,20 @@
 defmodule Publishex.Util do
+  @moduledoc """
+  Utility functions used by adapters
+  """
+
+  @doc """
+  Remove prefix from a string
+  """
   def remove_prefix(path, prefix) do
     base = byte_size(prefix)
     <<_::binary-size(base), rest::binary>> = path
     rest
   end
 
+  @doc """
+  Check keyword list for presence of key and raise with error otherwise
+  """
   def config_key(opts, key) do
     case Keyword.fetch(opts, key) do
       {:ok, value} ->
